ba24143f385a17f27f96731cb4fbe8c5beb2de28
albertgoncalves <goncalves.albert@gmail.com>
2019-12-13 01:22:37 -0500

    remove wasteful array-to-list conversion

--- day_03/src/io.ml
+++ day_03/src/io.ml
@@ -35,11 +35,13 @@ let print_grid (g : Terrain.grid) : unit =
     let rec loop (i : int) : unit =
         if (i < n) then
             begin
-                let w : char list =
-                    Array.sub g.Terrain.buffer i (min g.Terrain.width (n - i))
-                    |> Array.to_list in
+                let w : char array =
+                    Array.sub
+                        g.Terrain.buffer
+                        i
+                        (min g.Terrain.width (n - i)) in
                 let buf : Buffer.t = Buffer.create g.Terrain.width in
-                List.iter (Buffer.add_char buf) w;
+                Array.iter (Buffer.add_char buf) w;
                 Buffer.contents buf |> Printf.fprintf stdout "%s\n";
                 loop (i + g.Terrain.width)
             end
