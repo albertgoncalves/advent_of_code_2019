5bda85c4c0461f8f905f2422eec6f9b388f8c44f
albertgoncalves <goncalves.albert@gmail.com>
2019-12-12 21:57:26 -0500

    got it in 1

--- day_02/src/part1.ml
+++ day_02/src/part1.ml
@@ -1,14 +1,35 @@
 let rec pretty_print : int list -> unit = function
     | a :: b :: c :: d :: xs ->
-        Printf.fprintf stdout "%d\t%d\t%d\t%d\n" a b c d;
+        Printf.fprintf stdout "\t%d\t%d\t%d\t%d\n" a b c d;
         pretty_print xs
-    | a :: b :: c :: [] -> Printf.fprintf stdout "%d\t%d\t%d\n" a b c
-    | a :: b :: [] -> Printf.fprintf stdout "%d\t%d\n" a b
-    | a :: [] -> Printf.fprintf stdout "%d\n" a
+    | a :: b :: c :: [] -> Printf.fprintf stdout "\t%d\t%d\t%d\n" a b c
+    | a :: b :: [] -> Printf.fprintf stdout "\t%d\t%d\n" a b
+    | a :: [] -> Printf.fprintf stdout "\t%d\n" a
     | [] -> flush stdout
+
+let calculate (xs : int array) : int array =
+    xs.(1) <- 12;
+    xs.(2) <- 2;
+    let rec loop n i =
+        if (n <= i) || (xs.(i) = 99) then
+            xs
+        else
+            let j : int = xs.(i + 1) in
+            let k : int = xs.(i + 2) in
+            let l : int = xs.(i + 3) in
+            let _ : unit = match xs.(i) with
+                | 1 -> xs.(l) <- xs.(j) + xs.(k)
+                | 2 -> xs.(l) <- xs.(j) * xs.(k)
+                | _ -> () in
+            loop n (i + 4) in
+    loop (Array.length xs) 0
+
 
 let () : unit =
     input_line stdin
     |> String.split_on_char ','
     |> List.map int_of_string
+    |> Array.of_list
+    |> calculate
+    |> Array.to_list
     |> pretty_print
---
+++ day_02/src/part2.ml
@@ -0,0 +1,41 @@
+let calculate (xs : int array) (a : int) (b : int) : int array =
+    xs.(1) <- a;
+    xs.(2) <- b;
+    let rec loop (n : int) (i : int) : unit =
+        if (n <= i) || (xs.(i) = 99) then
+            ()
+        else
+            let j : int = xs.(i + 1) in
+            let k : int = xs.(i + 2) in
+            let l : int = xs.(i + 3) in
+            let _ : unit = match xs.(i) with
+                | 1 -> xs.(l) <- xs.(j) + xs.(k)
+                | 2 -> xs.(l) <- xs.(j) * xs.(k)
+                | _ -> () in
+            loop n (i + 4) in
+    loop (Array.length xs) 0;
+    xs
+
+let iterate (xs : int array) : int option =
+    let rec loop (a : int) (b : int) =
+        if (calculate (Array.copy xs) a b).(0) = 19690720 then
+            Some ((100 * a) + b)
+        else if a < 99 then
+            loop (a + 1) b
+        else if b < 99 then
+            loop 0 (b + 1)
+        else
+            None in
+    loop 0 0
+
+let maybe_print : int option -> unit = function
+    | Some x -> Printf.fprintf stdout "\t%d\n%!" x
+    | None -> ()
+
+let () : unit =
+    input_line stdin
+    |> String.split_on_char ','
+    |> List.map int_of_string
+    |> Array.of_list
+    |> iterate
+    |> maybe_print
