88c00a661ddf07c026909e72a9353f363b60b747
albertgoncalves <goncalves.albert@gmail.com>
2019-12-12 22:10:54 -0500

    clean-up, no-op

--- day_01/src/part1.ml
+++ day_01/src/part1.ml
@@ -1,14 +1,15 @@
 let read_file (filename : string) : string list =
     let lines : string list ref = ref [] in
     let chan : in_channel = open_in filename in
-    try
-        while true; do
-            lines := input_line chan :: !lines
-        done;
-        !lines
-    with End_of_file ->
-        close_in chan;
-        List.rev !lines
+    begin
+        try
+            while true; do
+                lines := input_line chan :: !lines
+            done
+        with End_of_file ->
+            close_in chan
+    end;
+    List.rev !lines
 
 let () : unit =
     try
--- day_01/src/part2.ml
+++ day_01/src/part2.ml
@@ -1,14 +1,15 @@
 let read_file (filename : string) : string list =
     let lines : string list ref = ref [] in
     let chan : in_channel = open_in filename in
-    try
-        while true; do
-            lines := input_line chan :: !lines
-        done;
-        !lines
-    with End_of_file ->
-        close_in chan;
-        List.rev !lines
+    begin
+        try
+            while true; do
+                lines := input_line chan :: !lines
+            done
+        with End_of_file ->
+            close_in chan
+    end;
+    List.rev !lines
 
 let calculate (x : int) : int = (x / 3) - 2
 
@@ -18,7 +19,7 @@ let iterate (x : int) : int =
             x
         else
             loop (x + y) (calculate y) in
-    calculate x |> loop 0
+    loop 0 (calculate x)
 
 let () : unit =
     try
