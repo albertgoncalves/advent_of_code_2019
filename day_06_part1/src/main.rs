325ba4625bbf4f49222414c7168c3cf2d334389f
albertgoncalves <goncalves.albert@gmail.com>
2019-12-15 00:58:12 -0500

    remove String allocations

--- day_06_part1/src/main.rs
+++ day_06_part1/src/main.rs
@@ -8,36 +8,36 @@ use std::process;
 
 #[allow(clippy::cast_precision_loss)]
 fn main() {
-    let mut links: HashMap<String, String> = HashMap::new();
+    let mut links: HashMap<&str, &str> = HashMap::new();
     let mut handler: File = File::open(env::args().nth(1).unwrap()).unwrap();
     let mut contents = String::new();
     handler.read_to_string(&mut contents).unwrap();
     for pair in contents.trim_end().split('\n') {
         if let [a, b] = pair.split(')').collect::<Vec<&str>>()[..] {
-            if links.insert(b.to_owned(), a.to_owned()).is_none() {
+            if links.insert(b, a).is_none() {
                 continue;
             }
         }
         process::exit(1);
     }
-    let mut depths: HashMap<String, u32> = HashMap::new();
+    let mut depths: HashMap<&str, u32> = HashMap::new();
     let mut result: u32 = 0;
     let mut cost: u32 = 0;
     for key in links.keys() {
         let mut n: u32 = 0;
-        let mut cursor: String = key.to_owned();
-        while let Some(link) = links.get(&cursor) {
+        let mut cursor: &str = key;
+        while let Some(link) = links.get(cursor) {
             cost += 1;
             n += 1;
             result += 1;
-            cursor = link.to_owned();
-            if let Some(m) = depths.get(&cursor) {
+            cursor = link;
+            if let Some(m) = depths.get(cursor) {
                 n += m;
                 result += m;
                 break;
             }
         }
-        depths.insert(key.to_owned(), n);
+        depths.insert(key, n);
     }
     println!(
         "{} {} {}",
--- day_06_part2/src/main.rs
+++ day_06_part2/src/main.rs
@@ -6,30 +6,32 @@ use std::fs::File;
 use std::io::Read;
 use std::process;
 
-fn trace(links: &HashMap<String, String>, start: &str) -> Vec<String> {
-    let mut path: Vec<String> = vec![start.to_owned()];
+fn trace<'a>(
+    links: &'a HashMap<&'a str, &'a str>,
+    start: &'a str,
+) -> Vec<&'a str> {
+    let mut path: Vec<&str> = vec![start];
     while let Some(next) = links.get(path.last().unwrap()) {
-        path.push(next.to_owned());
+        path.push(next);
     }
     path
 }
 
-#[allow(clippy::cast_precision_loss)]
 fn main() {
-    let mut links: HashMap<String, String> = HashMap::new();
+    let mut links: HashMap<&str, &str> = HashMap::new();
     let mut handler: File = File::open(env::args().nth(1).unwrap()).unwrap();
     let mut contents = String::new();
     handler.read_to_string(&mut contents).unwrap();
     for pair in contents.trim_end().split('\n') {
         if let [a, b] = pair.split(')').collect::<Vec<&str>>()[..] {
-            if links.insert(b.to_owned(), a.to_owned()).is_none() {
+            if links.insert(b, a).is_none() {
                 continue;
             }
         }
         process::exit(1);
     }
-    let mut a: Vec<String> = trace(&links, "YOU");
-    let mut b: Vec<String> = trace(&links, "SAN");
+    let mut a: Vec<&str> = trace(&links, "YOU");
+    let mut b: Vec<&str> = trace(&links, "SAN");
     while a.last().unwrap() == b.last().unwrap() {
         a.pop();
         b.pop();
